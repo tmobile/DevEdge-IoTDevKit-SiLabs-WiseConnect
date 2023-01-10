a68c20b868d87a954b055735697813695df2812c
Jared Baumann <jared.baumann8@t-mobile.com>
2023-01-10 09:16:00 -0700

    Code cleanup
    Removed examples(unused), resources (unused), and old license file
    
    Signed-off-by: Jared Baumann <jared.baumann8@t-mobile.com>

--- examples/snippets/wlan_bt_ble/wlan_https_bt_a2dp/platforms/nxp/rt595/board/rsi_board_configuration.h
+++
@@ -1,5 +0,0 @@
-#ifndef RSI_BOARD_CONFIG_H
-#define RSI_BOARD_CONFIG_H
-// Declaration of MCU type
-#define RSI_ARM_CM33
-#endif
--- examples/snippets/wlan_bt_ble/wlan_https_bt_a2dp_ble_dual_role/platforms/nxp/rt595/middleware/multicore/rpmsg_lite/doc/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['addr',['addr',['../group__rpmsg__lite.html#a28c0e9a7a70fbc58a497f2d3fac96f70',1,'rpmsg_lite_endpoint']]]
-];
